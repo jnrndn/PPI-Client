ac22e1edb9cdb2075748e54679a264726c8371dc
Juan José Rendón <juan.rendon@yuxiglobal.com>
2018-09-16 16:15:23 -0500

    feat: validate password strength

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { MatchError } from '../shared/shared';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatchError, passwordMatch, regexValidator } from '../helpers/shared';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: [ './registration.component.scss' ],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  passRegex = '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&].{7,}';
+
+  registrationGroup: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  confirmPassword: FormControl;
 
   matcher = new MatchError();
 
+  ngOnInit() {
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]);
+
+    this.password = new FormControl('', [
+      Validators.required,
+      regexValidator(new RegExp(this.passRegex), { 'strength': true }),
+    ]);
+
+    this.confirmPassword = new FormControl('', [
+      Validators.required,
+      passwordMatch(this.password, { 'match': true }),
+    ]);
+  }
 }
