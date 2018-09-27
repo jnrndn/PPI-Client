844562c98c24ae21b0167b9079152419a92d0dbd
Juan José Rendón <juan.rendon@yuxiglobal.com>
2018-09-26 23:17:32 -0500

    feat: get data from login form

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.scss'],
+  styleUrls: [ './authentication.component.scss' ],
 })
 export class AuthenticationComponent implements OnInit {
 
+  username: string;
+  password: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onLogin(formValue) {
+  }
 }
